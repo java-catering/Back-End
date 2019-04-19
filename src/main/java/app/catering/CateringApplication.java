f96cc25901ef87d013befd496a8fcc12eee7274a
Eric <eric_gomez_1@hotmail.com>
2019-04-19 15:07:17 -0700

    implemented boilerplate, imported all required dependencys

---
+++ src/main/java/app/catering/CateringApplication.java
@@ -0,0 +1,15 @@
+package app.catering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CateringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/catering/CateringApplicationTests.java
@@ -0,0 +1,18 @@
+package app.catering;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CateringApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
