89f193b8787d4b18d29003664878f348631dce52
Eric <eric_gomez_1@hotmail.com>
2019-04-19 21:08:19 -0700

    implemented a seeding file for mock data

---
+++ src/main/java/app/catering/models/Product.java
@@ -0,0 +1,5 @@
+package app.catering.models;
+
+public class Product
+{
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO role (name) VALUES ('client');
+INSERT INTO role (name) VALUES ('admin');
+
+INSERT INTO user (first_name, last_name, email, password, role_id) VALUES
+('Eric', 'Gomez', 'eric@example.com', '123', 2)
+
