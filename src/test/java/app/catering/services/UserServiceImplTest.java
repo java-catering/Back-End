6d1a5253eefb3d0b078267317e5ba84f3f6f2a79
Eric <eric_gomez_1@hotmail.com>
2019-04-22 13:29:16 -0700

    updated the gitignore file to encompass the target dir among other unnessecarry things.

---
+++ src/test/java/app/catering/services/UserServiceImplTest.java
@@ -0,0 +1,20 @@
+package app.catering.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @Test
+    void deleteById()
+    {
+    }
+}
