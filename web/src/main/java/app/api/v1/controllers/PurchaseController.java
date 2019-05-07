7ad9eff7e9347a31acd6edb2813ad1a63e474d10
Eric <eric_gomez_1@hotmail.com>
2019-05-06 23:34:37 -0700

    created the purchase controller

---
+++ web/src/main/java/app/api/v1/controllers/PurchaseController.java
@@ -0,0 +1,12 @@
+package app.api.v1.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PurchaseController
+{
+
+
+}
